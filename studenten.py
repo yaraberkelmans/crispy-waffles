10ac561dbf9dd52a34805983b0173fa7b7ae3d1a
Tijn2210 <tijnotjes@gmail.com>
2025-01-08 22:10:33 +0100

    Add files via upload

--- studenten.py
+++ studenten.py
@@ -39,4 +39,28 @@ with open("vakken.csv") as c:
         course = Course(course_name, lectures_n, tutorial_n, tutorial_cap, labs_n, labs_cap, e_students)
         course_list.append(course)
 
+with open("studenten_en_vakken.csv") as sv:
+    for line in sv:
+        split_data = line.split(',')
+        student_id = split_data[2]
+        courses = [split_data[3],split_data[4],split_data[5],split_data[6],split_data[7]]
+
+        student = Student(student_id, courses)
+        student_list.append(student)
+
+with open("zalen.csv") as z:
+    for line in z:
+        split_data = line.split(',')
+        room_id = split_data[0]
+        capacity = split_data[1]
+
+        location = Location(room_id, capacity)
+
+        location_list.append(location)
+
+
 print(course_list[1].course_name)
+
+print(student_list[1].student_id)
+
+print(location_list[1].room_id)
