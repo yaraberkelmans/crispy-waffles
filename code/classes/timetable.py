4022be9e02b1563c17371c4c90de4c1b242f2618
GrumbaBoi <maxvanbeusekom@icloud.com>
2025-01-09 15:17:24 +0100

    Added separate method to create timetable and initialized lists for days and times in the timetable __init__.

--- code/classes/timetable.py
+++ code/classes/timetable.py
@@ -2,6 +2,10 @@ from loading_data import Load_init_lists
 
 class Timetable():
     def __init__(self, days, times):
-        for day in days:
-            for time in times:
+        self.days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+        self.times = ['9-11', '11-13', '13-15', '15-17']
+
+    def create_timetable(self)
+        for day in self.days:
+            for time in self.times:
                 self.timetable[Timeslot(day, time)] = {}
