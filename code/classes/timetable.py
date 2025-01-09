770268aae4fb10e64152012cc7f1babbb1b992b3
Tijn2210 <tijnotjes@gmail.com>
2025-01-09 15:13:35 +0100

    added a docstring to timetable.py for clarification

--- code/classes/timetable.py
+++ code/classes/timetable.py
@@ -2,6 +2,7 @@ from loading_data import Load_init_lists
 
 class Timetable():
     def __init__(self, days, times):
+        """Inputs days and times are lists and for every combination, a timeslot instance is created to be used as the key for the dictionary, the value is an empty diictionary."""
         for day in days:
             for time in times:
                 self.timetable[Timeslot(day, time)] = {}
