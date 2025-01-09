746301046684e9b92302152f239312eea5dab6ac
GrumbaBoi <maxvanbeusekom@icloud.com>
2025-01-09 15:21:46 +0100

    Fixed indentation errors.

--- code/classes/timetable.py
+++ code/classes/timetable.py
@@ -2,7 +2,11 @@ from loading_data import Load_init_lists
 
 class Timetable():
     def __init__(self, days, times):
-         """Inputs days and times are lists and for every combination, a timeslot instance is created to be used as the key for the dictionary, the value is an empty diictionary."""
+        """
+        Inputs days and times are lists and for every combination, 
+        a timeslot instance is created to be used as the key for the dictionary, 
+        the value is an empty diictionary.
+        """
         self.days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
         self.times = ['9-11', '11-13', '13-15', '15-17']
 
