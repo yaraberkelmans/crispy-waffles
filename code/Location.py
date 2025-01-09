eed4d066a13c035c8a2580cfd510a41fa3297f46
yaraberkelmans <yaraberkelmans@gmail.com>
2025-01-09 14:03:31 +0100

    adding location and student classes files

---
+++ code/Location.py
@@ -0,0 +1,7 @@
+from .loading_data import Load_init_lists
+
+class Location():
+    def __init__(self, room_id, capacity):
+        self.room_id = room_id
+        self.capacity= capacity
+        self.available = True
---
+++ code/Student.py
@@ -0,0 +1,6 @@
+from .loading_data import Load_init_lists
+
+class Student():
+    def __init__(self, student_id, courses):
+        self.student_id = student_id
+        self.courses = courses
