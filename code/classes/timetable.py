34164197e819cbc287e1a35fb58dc7dfe923fcbf
GrumbaBoi <maxvanbeusekom@icloud.com>
2025-01-10 13:32:19 +0100

    Added method name_classes to timetable class to give each class a name.

--- code/classes/timetable.py
+++ code/classes/timetable.py
@@ -14,7 +14,7 @@ class Timetable():
         self.days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
         self.times = ['9-11', '11-13', '13-15', '15-17']
         self.course_classes = {}
-
+        self.classes_list = []
     def create_timetable(self):
         for day in self.days:
             for time in self.times:
@@ -38,11 +38,21 @@ class Timetable():
         for course in self.courses:
             course.count_groups()
             self.course_classes[course.course_name] = {'Lectures': course.lectures_n, 'Tutorials': course.expected_tut_n, 'Labs': course.expected_lab_n}
+
+    def name_classes(self):
+        self.classes_list = []
+        for course_name, classes_count_dict in self.course_classes.items():
+            for class_type, class_amount in classes_count_dict.items():
+                for i in range(class_amount):
+                    class_name = course_name + ' ' + class_type + ' ' + str(i+1)
+                    self.classes_list.append(class_name)
         
 
 print(os.getcwd())
 timetable = Timetable()
 timetable.load_courses('data/vakken.csv')
 timetable.get_classes_count()
+timetable.name_classes()
 print(timetable.courses[1].course_name)
-print(timetable.course_classes)
+print(timetable.classes_list)
+
