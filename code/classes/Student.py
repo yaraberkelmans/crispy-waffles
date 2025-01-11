08114a76ddcef8db2d7924f6ffde6099140246a2
GrumbaBoi <maxvanbeusekom@icloud.com>
2025-01-11 20:09:11 +0100

    Changed course_classes variable name from timetable class to classes_per_course for readability. Added TODOs to timetable classes to suggest future changes in code design. Made line 45 more readable by using more lines for dictionary. Updated add_students_to_class method from timetable class to return instantly if class is not in the classes list. Added variables to student class self for first name, last name, full name and personal timetable.

--- code/classes/Student.py
+++ code/classes/Student.py
@@ -1,8 +1,12 @@
 from loading_data import Load_init_lists
 
 class Student():
-    def __init__(self, student_id, courses):
+    def __init__(self, student_id, courses, first_name, last_name):
         self.student_id = student_id
         self.courses = set(courses)
         self.is_valid = True
         self.placed = False    
+        self.first_name = first_name
+        self.last_name = last_name
+        self.name = f'{first_name} {last_name}'
+        self.timetable = {}
--- code/classes/timetable.py
+++ code/classes/timetable.py
@@ -14,15 +14,16 @@ class Timetable():
         self.days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday']
         self.times = ['9-11', '11-13', '13-15', '15-17']
         self.locations = ['A1.04', 'A1.06', 'A1.08', 'A1.10','B0.201', 'C0.110', 'C1.112']
-        self.course_classes = {}
-        self.classes_list = []
+        self.classes_per_course = {}
+        self.classes_list = {}
         self.timetable = {}
     
     def create_timetable(self):
         for day in self.days:
             for time in self.times:
                 self.timetable[Timeslot(day, time).name] = {}
-        
+    
+    # TODO: incorporate this function in create timetable or init func in a handy way to simplify (defaultdict maybe)
     def initialize_locations(self):
         for timeslot in self.timetable.keys():
             for location in self.locations:
@@ -42,24 +43,27 @@ class Timetable():
     def get_classes_count(self):
         for course in self.courses:
             course.count_groups()
-            self.course_classes[course.course_name] = {'Lecture': course.lectures_n, 'Tutorial': course.expected_tut_n, 'Lab': course.expected_lab_n}
+            self.classes_per_course[course.course_name] = {'Lecture': course.lectures_n,
+                                                           'Tutorial': course.expected_tut_n,
+                                                           'Lab': course.expected_lab_n}
 
-    def name_classes(self):
-        self.classes_list = {}
+    def name_classes(self):  
+        # TODO: Make it list comprehension instead of loop
         for course_name, classes_count_dict in self.course_classes.items():
             for class_type, class_amount in classes_count_dict.items():
                 for i in range(class_amount):
-                    class_name = course_name + ' ' + class_type + ' ' + str(i+1)
+                    class_name = f'{course_name} {class_type} {i+1}'
                     self.classes_list[class_name] = []
 
     def add_student_to_class(self, student, class_name):
-        if class_name in self.classes_list.keys():
-            if student not in self.classes_list[class_name]:
-                self.classes_list[class_name].append(student)
-            else:
-                print('Student already in Class')
+        if class_name not in self.classes_list.keys():
+            print(f'Class {class_name} does not exist.')
+            return
+        
+        if student not in self.classes_list[class_name]:
+            self.classes_list[class_name].append(student)
         else:
-            print('Class does not exist')
+            print(f'Student {student.name} already in Class {class_name}.')
         
 
 print(os.getcwd())
