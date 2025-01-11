f59b08f0eac73ac8f3d0f41ae556f21ed52e912a
GrumbaBoi <maxvanbeusekom@icloud.com>
2025-01-11 20:22:15 +0100

    Added a todo for locations.

--- code/classes/Location.py
+++ code/classes/Location.py
@@ -3,5 +3,5 @@ from loading_data import Load_init_lists
 class Location():
     def __init__(self, room_id, capacity):
         self.room_id = room_id
-        self.capacity= capacity
+        self.capacity = capacity
         self.available = True
