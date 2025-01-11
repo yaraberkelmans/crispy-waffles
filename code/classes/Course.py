4a29e54a9e269964e2f5636f5932400084108eed
Tijn2210 <tijnotjes@gmail.com>
2025-01-11 20:55:29 +0100

    added docstrings to function in course.py

--- code/classes/Course.py
+++ code/classes/Course.py
@@ -14,6 +14,8 @@ class Course():
             self.tutorial_cap = int(tutorial_cap)
         if labs_cap:
             self.labs_cap = int(labs_cap)
+
+        # only create a dictionary if there are tutorials or labs for this course
         if int(self.tutorial_n) >= 1:
             self.tutorials = {}
         if int(self.labs_n) >= 1:
@@ -21,7 +23,7 @@ class Course():
 
     def add_students_tut(self, student_list):
         """This method adds a certain list of students to seperate tutorial groups, based on the tutorials_cap per tutorial group. The tutorials are in a dictionary with the
-        tutorial # as key and a list of students as values"""
+        tutorial # as key and a list of students as values."""
         for x in range(1,self.expected_tut_n+1):
             stud_per_tut = len(student_list)/ self.expected_tut_n
             self.tutorials[x] = student_list[round(((x-1) * stud_per_tut)):round((x *stud_per_tut))]
@@ -29,18 +31,20 @@ class Course():
 
     def add_students_lab(self, student_list):
         """This method adds a certain list of students to seperate lab groups, based on the labs_cap per lab group. The labs are in a dictionary with the
-        lab # as key and a list of students as values"""
+        lab # as key and a list of students as values."""
         for x in range(1,self.expected_lab_n+1):
             stud_per_lab = len(student_list)/ self.expected_lab_n
             self.labs[x] = student_list[round(((x-1) * stud_per_lab)):round((x *stud_per_lab))]
         print(len(self.labs.keys()))
 
     def add_actual_students(self):
+        """This method adds a student to the course, based on the courses list in a Student instance."""
         for student in self.all_student_list:
             if self.course_name in student.courses:
                 self.student_list.append(student.student_id)
 
     def add_individual_student(self, student):
+        """This method adds an individual student to the student list of the course."""
         if student not in self.student_list: 
             self.student_list.append(student)
         else:
@@ -50,6 +54,7 @@ class Course():
         pass
 
     def count_groups(self):
+        """This method calculates the amount of tutorial and lab groups needed to fit all the expected students."""
         if self.tutorial_n > 0:
             self.expected_tut_n = self.e_students // self.tutorial_cap
             if self.e_students % self.tutorial_cap > 0:
