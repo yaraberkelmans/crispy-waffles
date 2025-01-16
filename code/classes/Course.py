4f50738be10a004126a3974bd25c43401569ec62
Tijn2210 <tijnotjes@gmail.com>
2025-01-16 14:17:40 +0100

    adjusted indentation in course.py

--- code/classes/Course.py
+++ code/classes/Course.py
@@ -1,9 +1,9 @@
 import csv
 
 class Course():
-     """
-     This class represents a course with lectures, tutorials, labs, and students.
-     """
+    """
+    This class represents a course with lectures, tutorials, labs, and students.
+    """
     def __init__(self, course_name, lectures_n=0, tutorial_n=0, tutorial_cap=0, lab_n=0, lab_cap=0, e_students=0):
         self.course_name = course_name
         self.lectures_n = int(lectures_n)
@@ -30,21 +30,27 @@ class Course():
         print(len(self.tutorials.keys()))
 
     def add_students_lab(self, student_list):
-        """This method adds a certain list of students to seperate lab groups, based on the lab_cap per lab group. The labs are in a dictionary with the
-        lab number as key and a list of students as values."""
+        """
+        This method adds a certain list of students to seperate lab groups, based on the lab_cap per lab group. The labs are in a dictionary with the
+        lab number as key and a list of students as values.
+        """
         for x in range(1,self.expected_lab_n+1):
             stud_per_lab = len(student_list)/ self.expected_lab_n
             self.labs[x] = student_list[round(((x-1) * stud_per_lab)):round((x *stud_per_lab))]
         print(len(self.labs.keys()))
 
     def add_actual_students(self):
-        """This method adds a student to the course, based on the courses list in a Student instance."""
+        """
+        This method adds a student to the course, based on the courses list in a Student instance.
+        """
         for student in self.all_student_list:
             if self.course_name in student.courses:
                 self.student_list.append(student)
 
     def add_individual_student(self, student):
-        """This method adds an individual student to the student list of the course."""
+        """
+        This method adds an individual student to the student list of the course.
+        """
         if student not in self.student_list and self not in student.pers_activities: 
             self.student_list.append(student)
 
@@ -79,13 +85,12 @@ class Course():
             self.expected_lab_n = 0
             
     def __repr__(self) -> str:
-        """
-        This method returns a string representation of the course name.
-        """
         return f"{self.course_name}"
 
 class Tutorial():
-      """This class epresents a tutorial group within a course."""
+    """
+    This class epresents a tutorial group within a course.
+    """
     def __init__(self, course, tutorial_cap, name):
         self.course = course  
         self.capacity = tutorial_cap
