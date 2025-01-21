ba366d4c102bc3c27d1c488fb831744ef958f8b2
Tijn2210 <tijnotjes@gmail.com>
2025-01-21 15:37:24 +0100

    changes in exp and filled a timetable

--- code/classes/experiment.py
+++ code/classes/experiment.py
@@ -1,3 +1,5 @@
+from code.algorithms.randomize import randomize
+
 class Experiment():
     def __init__(self, timetable, iterations):
         self.timetable = timetable
@@ -6,6 +8,14 @@ class Experiment():
     def run_algorithm(self, algorithm, alg_iters):
         results= []
         for iter in range(self.iterations):
+            randomized_timetable = randomize(self.timetable)
             result= algorithm.run(self.timetable, alg_iters)
             results.append(result)
-            
+
+        scores = [res["score"] for res in results]
+        summary = {"best_score": max(scores),
+                    "average_score": sum(scores) / len(scores),
+                    "all_scores": scores}
+        
+        return summary
+
