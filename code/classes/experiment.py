3dfd88eb0ccc1d22764e5424772c199256c8492b
GrumbaBoi <maxvanbeusekom@icloud.com>
2025-01-21 22:24:37 +0100

    added export to results.csv added iteration counter for export and probably everything is bugged because score is 11

--- code/classes/experiment.py
+++ code/classes/experiment.py
@@ -1,12 +1,14 @@
 from code.algorithms.randomize import randomize
-
+import csv
+import pickle
 class Experiment():
     def __init__(self, timetable, iterations):
         self.timetable = timetable
         self.iterations = iterations
-
+        self.results = []
+    
     def run_algorithm(self, algorithm, alg_iters):
-        results= []
+        self.results= []
         for iter in range(self.iterations):
             randomized_timetable = randomize(self.timetable)
             result= algorithm.run(self.timetable, alg_iters)
@@ -19,3 +21,8 @@ class Experiment():
         
         return summary
 
+    def export_results(self):
+        with open ('Results.csv', "a", newline='') as f:
+            writer = csv.writer(f)
+
+            writer.writerow(self.results)
