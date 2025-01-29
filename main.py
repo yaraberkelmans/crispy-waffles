1308ee2494b254d2919dfbb45bcc418256e638ea
Tijn2210 <tijnotjes@gmail.com>
2025-01-29 16:56:32 +0100

    extra runn of n7s1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 
 from code.classes.timetable import Timetable
 from code.algorithms.malus import calculate_malus
-from code.algorithms.hill_climber import HillClimber
+from code.algorithms.hill_climber_advanced import HillClimber
 from code.visualize.visualize import *
 from code.classes.experiment import Experiment
 from code.algorithms.simulated_annealing import SimulatedAnnealing
@@ -34,18 +34,16 @@ if __name__ == "__main__":
     
 
     # -------------------------------------------------------- Hill Climber -----------------------------------------------------
-    # run Hill Climber
     if args.algorithm == 'HillClimber':
 
-        hill_climber_summary = experiment.run_algorithm( HillClimber, args.output_file_path, verbose=True, verbose_alg=False, 
+        hill_climber_summary = experiment.run_algorithm(HillClimber, args.output_file_path, verbose=True, verbose_alg=False, 
                                                    neighbours=args.neighbours, swaps_per_neighbour=args.swaps, 
                                                    iterations=args.iterations)
         print("Hill Climber Summary:", hill_climber_summary)
         print('Malus per cat', experiment.malus_per_cat)
 
 
-    # -------------------------------------------------------Simulated Annealing -----------------------------------------------
-    # run SimAnn
+    # ----------------------------------------------------- Simulated Annealing -------------------------------------------------
     if args.algorithm == "SimulatedAnnealing":
         sim_ann_summary = experiment.run_algorithm(SimulatedAnnealing, args.output_file_path, verbose=True, verbose_alg=False, 
                                                    neighbours=args.neighbours, swaps_per_neighbour=args.swaps, 
@@ -54,7 +52,7 @@ if __name__ == "__main__":
         print('Malus per cat', experiment.malus_per_cat)
 
 
-    # # ---------------------------------- Format for loading in timetable and exporting to csv -----------------------------------
+    # # ---------------------------------- Format for loading in timetable and exporting to csv ---------------------------------
     # input_file_path = None
     # stored_timetable = load_pickle_file(input_file_path)
 
