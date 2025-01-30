44e79e3cde7e80e27312a89a8a81b3bdaa9cf124
Tijn2210 <tijnotjes@gmail.com>
2025-01-30 14:48:18 +0100

    now made sure it actually works

--- code/classes/experiment.py
+++ code/classes/experiment.py
@@ -23,6 +23,8 @@ class Experiment():
         # initialize with none and inf to be sure the first timetable always overwrites the variables
         self.best_timetable = None
         self.best_score = float('inf')
+        self.malus_per_cat_list = []
+        self.all_timetables = defaultdict(list)
         
     
     def run_algorithm(self, algorithm_class, folder_path='data/', file_name_addition = '', verbose = False, temperature=None, **algorithm_params):
@@ -34,19 +36,10 @@ class Experiment():
         """
         self.results = []  
         
+        # setup output_file_name for exporting
         self.check_folder_existence(folder_path)
         self.alg_params = algorithm_params
-
-        # create a format for output file name based on the algorithm params
-        params_string = '_'.join(f"{value}_{key}" for key, value in algorithm_params.items() if key != 'verbose_alg')
-
-        # for Simulated Annealing we can also modify temperature so we need to add it to the file name
-        if temperature:
-            file_name_addition += f'_Temp={temperature}'
-
-        self.output_file_name = f'{folder_path}{algorithm_class.__name__}_{params_string}_{file_name_addition}'
-        self.malus_per_cat_list = []
-        self.all_timetables = defaultdict(list)
+        self.create_output_file_name(algorithm_class, folder_path, file_name_addition, temperature, **algorithm_params)
 
         for iter in range(self.iterations):
             if verbose:
@@ -54,27 +47,12 @@ class Experiment():
 
             self.malus_per_cat = {'capacity': 0, 'evening':  0, 'indiv_confl': 0, 'gap_hours': 0}
 
-            # create a randomized starting timetable before running the algorithm
-            randomize_algorithm = Randomize(self.timetable)
-            randomized_timetable = randomize_algorithm.run()
+            # randomize a timetable 
+            randomize_algorithm, randomized_timetable = self.run_randomize()
 
-            
-            if algorithm_class != Randomize:
-                if algorithm_class == SimulatedAnnealing or algorithm_class == GeneticSimulatedAnnealing:
-                    algorithm = algorithm_class(randomized_timetable, temperature=temperature)
-                else:
-                    algorithm = algorithm_class(randomized_timetable)
+            # run an iterative algorithm and get the score and algorithm instance
+            score, algorithm = self.run_additional_algorithm(algorithm_class, randomize_algorithm, randomized_timetable, iter, temperature=temperature, **algorithm_params)
 
-                score = algorithm.run(**algorithm_params)
-                self.all_timetables[iter].append(algorithm.timetable)
-
-                # add a dictionary to the list with malus points per iteration for each algorithm run
-                self.indiv_scores.append(algorithm.iteration_values)
-
-            else:
-                algorithm = randomize_algorithm
-                score = calculate_malus(randomized_timetable)
-               
             # save the result for this iteration
             self.results.append({"iteration": iter, "score": score})
 
@@ -114,6 +92,7 @@ class Experiment():
 
         return self.summary
 
+
     def export_results(self):
         """ 
         This method exports self.results as a csv file.
@@ -121,6 +100,7 @@ class Experiment():
         with open (f'{self.output_file_name}_Results.csv', "a", newline='') as f:
             writer = csv.writer(f)
             writer.writerow(self.results)
+
 
     def update_total_malus(self, algorithm):
         """
@@ -132,6 +112,7 @@ class Experiment():
         self.malus_per_cat['indiv_confl'] = check_individual_conflicts(algorithm.timetable)
         self.malus_per_cat['gap_hours'] = check_gap_hours(algorithm.timetable)
         self.malus_per_cat_list.append(self.malus_per_cat)
+
 
     def calculate_average_malus(self):
         """
@@ -159,4 +140,57 @@ class Experiment():
         """
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)  
+    
+
+    def run_additional_algorithm(self, algorithm_class, randomize_algorithm, randomized_timetable, iter, temperature, **algorithm_params):
+        """
+        This method runs an additional algorithm if the algorithm_class is not randomize. Else it returns 
+        randomize_algorithm as algorithm and calculates the score of the intitial randomized_timetable. 
+        If it runs an additional algorithm, it keeps track of the individual score per iteration and 
+        appends each timetable to the all timetables dictionary. Returns the score and the algorithm.
+        """
+        if algorithm_class != Randomize:
+
+            # only use temperature argument if the algorithm is a form of Simulated Annealing
+            if algorithm_class == SimulatedAnnealing or algorithm_class == GeneticSimulatedAnnealing:
+                algorithm = algorithm_class(randomized_timetable, temperature=temperature)
+            else:
+                algorithm = algorithm_class(randomized_timetable)
+
+            # starts the run method of the algorithm
+            score = algorithm.run(**algorithm_params)
+            self.all_timetables[iter].append(algorithm.timetable)
+
+            # add a dictionary to the list with malus points per iteration for each algorithm run
+            self.indiv_scores.append(algorithm.iteration_values)
+
+        else:
+            algorithm = randomize_algorithm
+            score = calculate_malus(randomized_timetable)
         
+        return score, algorithm
+    
+
+    def run_randomize(self):
+        """
+        This method runs randomize to get a randomized_timetable as starting point and returns that timetable.
+        """
+        randomize_algorithm = Randomize(self.timetable)
+        randomized_timetable = randomize_algorithm.run()
+
+        return randomize_algorithm, randomized_timetable
+
+
+    def create_output_file_name(self, algorithm_class, folder_path, file_name_addition, temperature, **algorithm_params):
+        """ 
+        This method creates an output_file_name for th experiment based on the algorithm parameters. 
+        It stores this output_file_name in self.
+        """
+        # transform the parameters to a string
+        params_string = '_'.join(f"{value}_{key}" for key, value in algorithm_params.items() if key != 'verbose_alg')
+
+        # for Simulated Annealing we can also modify temperature so we need to add it to the file name
+        if temperature:
+            file_name_addition += f'_Temp={temperature}'
+
+        self.output_file_name = f'{folder_path}{algorithm_class.__name__}_{params_string}_{file_name_addition}'
